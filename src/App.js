459d375307a956a39b4f09938d367ff5628d5095
abhisheksinghabsk <abhisheksingh8604211348@gmail.com>
2022-12-04 17:34:45 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import './App.css';
+let adjustInterval =undefined;
+
+function App() {
+const [watch,setWatch]=useState(0)
+const [started,setStarted] = useState(false)
+
+const startWatch=()=>{
+  adjustInterval = setInterval(()=>{
+  setWatch((x)=>x+1);
+},1000)
+setStarted(true)
+}
+const stopWatch=()=>{
+clearInterval (adjustInterval)
+setStarted(false)
+}
+const resetWatch=()=>{
+setWatch(0)
+clearInterval (adjustInterval)
+setStarted(false)
+
+}
+  return (
+    <div className='App'>
+      <h1>Stop Watch</h1>
+      <h1 className='watch'>{watch}</h1>
+      <button className='start' disabled={started} onClick={startWatch}>Start</button>
+      <br />
+      <button className='stop' onClick={stopWatch}>Stop</button>
+      <br />
+      <button className='reset' onClick={resetWatch}>Reset</button>
+      
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Login.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+function Login() {
+  return (
+    <div>
+      <h1>Stop Watch</h1>
+      <h1>0</h1>
+      <button>Start</button>
+      <button>Stop</button>
+      <button>Reset</button>
+      
+
+    </div>
+  );
+}
+export default Login;
